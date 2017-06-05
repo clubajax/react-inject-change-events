98569c104dfaff03dc0b37dce4bffc9adcf12cf6
Mike Wilcox <anm9tr@gmail.com>
2017-06-05 11:59:33 -0500

    prevent double events

--- index.js
+++ index.js
@@ -22,6 +22,21 @@ var acceptedNodeNames = {
 	input: true
 };
 
+var timeout;
+var lastNodeFired;
+function justFired (targetNode) {
+	var result = false;
+  	if(targetNode === lastNodeFired){
+  		result = true;
+	}
+	lastNodeFired = targetNode;
+  	clearTimeout(timeout);
+	timeout = setTimeout(function() {
+		lastNodeFired = null;
+	}, 100);
+	return result;
+}
+
 function injectChangeEvents (nodeNameArray) {
     nodeNameArray.forEach(function (nodeName) {
         acceptedNodeNames[nodeName] = true;
@@ -65,8 +80,10 @@ function extractEvents (topLevelType, targetInst, nativeEvent, nativeEventTarget
 
     var getTargetInstFunc, handleEventFunc;
     // prevent child elements from emitting events
-    if (shouldUseChangeEvent(targetNode) && shouldUseChangeEvent(nativeEvent.target)) {
-    	getTargetInstFunc = getTargetInstForChangeEvent;
+    if (shouldUseChangeEvent(targetNode) && shouldUseChangeEvent(nativeEvent.target) && nativeEvent.type === 'change') {
+    	if(!justFired(targetNode)){
+    		getTargetInstFunc = getTargetInstForChangeEvent;
+    	}
 
     } else if (isTextInputElement(targetNode)) {
         getTargetInstFunc = getTargetInstForInputEvent;
